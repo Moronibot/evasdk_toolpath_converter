4a120025356edbacbe72ef231fddd77d59a8ee86
Euan <Euanmeston@gmail.com>
2021-06-25 12:13:50 +0100

    Setup and Makefile now accommodate second workflow

--- setup.py
+++ setup.py
@@ -4,7 +4,10 @@ is set within the workflow. Should catch most deep folder paths that should crop
 """
 import os
 
-WORKFLOW_PATH = 'Convert_Toolpath.workflow/Contents/document.wflow'
+FILE_CONVERTER_PATH = 'Convert_Toolpath.workflow/Contents/document.wflow'
+SAVE_CONVERTER_PATH = 'Save_Convert_Toolpath.workflow/Contents/document.wflow'
+
+WORKFLOW_LIST = [FILE_CONVERTER_PATH, SAVE_CONVERTER_PATH]
 
 PATH_TO_EXE = "/".join(os.getcwd().split('/')[3::])
 CMD_PATH = f"./{PATH_TO_EXE}/dist/converter $@"
@@ -12,17 +15,18 @@ STR_TO_REPLACE = "INSERT_CMD_STR"
 
 def set_command_string_in_workflow():
     """ Searches for STR_TO_REPLACE and writes the correct path to the file """
-    with open(WORKFLOW_PATH, "r") as workflow_file:
-        loaded_workflow = workflow_file.readlines()
-        workflow_file.close()
+    for workflow in WORKFLOW_LIST:
+        with open(workflow, "r") as workflow_file:
+            loaded_workflow = workflow_file.readlines()
+            workflow_file.close()
 
-    for n_line, file_contents in enumerate(loaded_workflow):
-        if STR_TO_REPLACE in file_contents:
-            loaded_workflow[n_line] = file_contents.replace(STR_TO_REPLACE, CMD_PATH)
+        for n_line, file_contents in enumerate(loaded_workflow):
+            if STR_TO_REPLACE in file_contents:
+                loaded_workflow[n_line] = file_contents.replace(STR_TO_REPLACE, CMD_PATH)
 
-    with open(WORKFLOW_PATH, "w") as workflow_file:
-        workflow_file.writelines(loaded_workflow)
-        workflow_file.close()
+        with open(workflow, "w") as workflow_file:
+            workflow_file.writelines(loaded_workflow)
+            workflow_file.close()
 
 
 if __name__ == '__main__':
